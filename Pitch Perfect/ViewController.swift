a8bd5a98630fa90cc005f2f7f2f89ebb6fcf5e6b
mateisuica <matei.suica@yahoo.com>
2015-10-01 21:22:03 +0300

    lesson 3b

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO Actually record
+        print("Start recording")
+        recordingInProgress.hidden = false
+        stopButton.hidden = false
+        recordButton.enabled = false
+        
+    }
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
+    @IBAction func stopAudio(sender: UIButton) {
+        print("Stop recording")
+        recordingInProgress.hidden = true
+        stopButton.hidden = true
+        recordButton.enabled = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +39,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var recordingInProgress: UILabel!
 
 }
 
